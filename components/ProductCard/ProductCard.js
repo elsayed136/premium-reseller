3efee53e61eb2444412e434fa9b0450e42080207
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-09-16 06:53:52 +0200

    completing products preview component.

---
+++ components/ProductCard/ProductCard.js
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import classes from './ProductCard.module.scss';
+
+const ProductCard = ({ product }) => {
+	return (
+		<div className={classes.product}>
+			<div className={classes.product__img}>
+				<Image
+					src={product.img[0]}
+					width={220}
+					height={220}
+					layout='responsive'
+				/>
+			</div>
+			<p className={classes.product__title}>{product.name}</p>
+			<p className={classes.product__price}>
+				{product.price !== product.priceAfterdiscount && (
+					<strong className={classes.product__discount}>
+						${product.price.toFixed(2)}
+					</strong>
+				)}
+
+				<strong
+					className={`${
+						product.price !== product.priceAfterdiscount &&
+						classes.product__price_old
+					}`}
+				>
+					${product.price.toFixed(2)}
+				</strong>
+			</p>
+			<span
+				className={`${classes.available} ${
+					product.inStock ? classes.in_stoke : classes.out_stoke
+				}`}
+			>
+				{product.inStock ? 'In Stock' : 'Out of Stock'}
+			</span>
+		</div>
+	);
+};
+
+export default ProductCard;
--- components/ProductsPreview/ProductsPreview.js
+++ components/ProductsPreview/ProductsPreview.js
@@ -1,9 +1,9 @@
-import Image from 'next/image';
 import ArrowLeft from '../../assets/imgs/arrow-left@2x.png';
 import ArrowRight from '../../assets/imgs/arrow-right@2x.png';
 import Container from '../Container/Container';
 import Dot from '../Dot/Dot';
 import IconButton from '../IconButton/IconButton';
+import ProductCard from '../ProductCard/ProductCard';
 import classes from './ProductsPreview.module.scss';
 
 const ProductsPreview = ({ products }) => {
@@ -12,21 +12,11 @@ const ProductsPreview = ({ products }) => {
 			<Container>
 				<h1 className={classes.title}>Explore Products</h1>
 				<div className={classes.preview}>
-					<div className='product'>
-						<div
-							className='prodcut__img'
-							style={{ position: 'relative', width: '220px', height: '220px' }}
-						>
-							<Image src={products[0].img[0]} layout='fill' />
-						</div>
-						<p className='product__title'>{products[0].name}</p>
-						<p>
-							<strong>${products[0].price}</strong>
-						</p>
-						<span className='product__price'>
-							{products[0].inStock ? 'In Stock' : 'Out of Stock'}
-						</span>
-					</div>
+					{products
+						.filter((_, i) => i < 4)
+						.map(product => (
+							<ProductCard key={product.id} product={product} />
+						))}
 				</div>
 				<div className={classes.pagination}>
 					<IconButton icon={ArrowLeft} />
