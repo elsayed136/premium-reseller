e98dcb2bff7ae6e0a6ac64724c6b0da701d4da0a
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-09-17 05:47:03 +0200

    refactor and adding document title

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Footer from '../Footer/Footer';
 import MainNavigation from '../MainNavigation/MainNavigation';
 import classes from './Layout.module.scss';
@@ -5,6 +6,10 @@ import classes from './Layout.module.scss';
 function Layout(props) {
 	return (
 		<>
+			<Head>
+				<title>Premium Resller</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			</Head>
 			<MainNavigation />
 			<main>{props.children}</main>
 			<Footer />
